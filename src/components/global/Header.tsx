6cd6dcf5f3969ed4c41420c347c9521282315629
akash <akcse16@gmail.com>
2023-09-02 00:33:55 +0530

    minor updates

--- src/components/global/Header.tsx
+++ src/components/global/Header.tsx
@@ -8,7 +8,7 @@ const Header = (props: HeaderProps) => {
 	const { title, showTimer = true } = props;
 	const [timeLeft, setTimeLeft] = useState(30 * 60);
 	const navigate = useNavigate();
-	
+
 	useEffect(() => {
 		if (timeLeft > 0) {
 			const timerInterval = setInterval(() => {
@@ -21,6 +21,42 @@ const Header = (props: HeaderProps) => {
 		}
 	}, [timeLeft, navigate]);
 
+	// Function to show an alert
+	const showAlert = () => {
+		window.alert(
+			"Are you sure you want to leave this page? You will be logged out and your data will be lost"
+		);
+	};
+
+	// Add event listeners to detect the refresh action
+	useEffect(() => {
+		const handleBeforeUnload = (event: any) => {
+			// Show the alert when the refresh action is detected
+			showAlert();
+			// Cancel the page reload by returning a confirmation message
+			event.returnValue = "Are you sure you want to leave this page?";
+			localStorage.clear();
+		};
+
+		const handleKeyDown = (event: any) => {
+			// Detect Ctrl + R (or Command + R on Mac) keyboard shortcut
+			if ((event.ctrlKey || event.metaKey) && event.key === "r") {
+				// Prevent the default browser refresh behavior
+				event.preventDefault();
+				// Show the alert
+				showAlert();
+			}
+		};
+
+		window.addEventListener("beforeunload", handleBeforeUnload);
+		window.addEventListener("keydown", handleKeyDown);
+
+		return () => {
+			// Clean up the event listeners when the component unmounts
+			window.removeEventListener("beforeunload", handleBeforeUnload);
+			window.removeEventListener("keydown", handleKeyDown);
+		};
+	}, []);
 	return (
 		<div className="header">
 			<img src="/images/josh-logo.svg" alt="" />
